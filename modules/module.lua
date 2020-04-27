23729b2db273fb948a0aacfdb62acf19c7450c59
fatboy <fatboychummy@gmail.com>
2020-04-26 22:21:56 -0600

    Initial update

--- modules/module.lua
+++ modules/module.lua
@@ -29,6 +29,39 @@ local modules = {
   }
 }
 
+-- -- -- Utility functions which cannot be loaded externally -- -- --
+
+-- get user input
+local function request(question, wrong, answers)
+  local flag = false
+  while true do
+    print(flag and wrong or question)
+    local input = string.lower(io.read())
+    for i = 1, #answers do
+      if input == answers[i] then
+        return input
+      end
+    end
+  end
+end
+
+-- Download file
+local function download(from, to)
+  local h, err = http.get(from)
+  if h then
+    local data = h.readAll()
+    h.close()
+    local h2, err2 = io.open(to, 'w')
+    if h2 then
+      h2:write(data):close()
+    else
+      error(string.format("Failed to open file '%s' due to '%s'.", tostring(to), tostring(err2)), 2)
+    end
+  else
+    error(string.format("Failed to connect to '%s' due to '%s'.", tostring(from), tostring(err)), 2)
+  end
+end
+
 -- deep copy function, for protection of resources.
 local function dCopy(x)
   -- if we aren't copying a table, return nothing.
@@ -82,6 +115,42 @@ function module.getDependencies(mod)
 
   -- no module
   error(string.format("No module %s in storage.", tostring(mod)), 2)
+end
+
+
+local function doMove(tempFile, saveTo)
+  fs.delete(saveTo)
+  fs.move(tempFile, saveTo)
+end
+-- update a module
+function module.update(mod, force)
+  -- determine if a module was passed or a string name of module was passed
+  local p = type(mod) == "table" and mod
+         or type(mod) == "string" and module.get(mod)
+         or error(string.format("Bad argument #1, expected string or table, got %s", type(mod)))
+  if p then
+    local tempFileN = string.format("/.temp%d", math.random(1, 100000))
+    fs.delete(tempFileN)
+    download(p.location, tempFileN)
+    local netInfo = loadfile(tempFileN)(true)
+    local currentInfo = loadfile(p.saveas)(true)
+    if netInfo._BUILD > currentInfo._BUILD then
+      if force then
+        doMove(tempFileN, p.saveas)
+      else
+        local ans = request(
+          string.format("Would you like to update file '%s'?", p.saveas),
+          "Please answer yes or no.",
+          {"yes", "no", "y", "n"}
+        )
+        if ans == "y" or ans == "yes" then
+          doMove(tempFileN, p.saveas)
+        end
+      end
+    end
+  else
+    error(string.format("Module '%s' does not exist!", tostring(mod)), 2)
+  end
 end
 
 return module
